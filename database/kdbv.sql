ff32bebe14843a0e88878a9a6017dbb1ef51e449
maksudkhan2232 <maksud.khan006@gmail.com>
2022-07-30 17:54:44 +0530

    Update kdbv.sql

--- database/kdbv.sql
+++ database/kdbv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 29, 2022 at 03:17 PM
+-- Generation Time: Jul 30, 2022 at 02:24 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
